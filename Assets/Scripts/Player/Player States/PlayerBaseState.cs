08856f43ab76a115cf211552f897b0113492e174
Paolo S <pabster4485@gmail.com>
2020-10-02 22:59:08 -0400

    added Finite State Machine for player and restructured files/folders

---
+++ Assets/Scripts/Player/Player States/PlayerBaseState.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class PlayerBaseState
+{
+    public abstract void EnterState(PlayerController player);
+    public abstract void Update(PlayerController player);
+    public abstract void OnCollisionEnter2D(PlayerController player);
+
+}
---
+++ Assets/Scripts/Player/Player States/PlayerIdleState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    
+    public override void EnterState(PlayerController player)
+    {
+        player._animator.Play(player.PLAYER_IDLE);
+    }
+
+    public override void OnCollisionEnter2D(PlayerController player)
+    {
+
+    }
+
+    public override void Update(PlayerController player)
+    {
+        if (player._isInHitstun)
+        {
+            player.TransitionToState(player.HitstunState);
+        }
+        if (player._isJumping)
+        {
+            player.TransitionToState(player.JumpingState);
+        }
+        if (Mathf.Abs(player._rigidBody.velocity.x) > 0.01)
+        {
+            player.TransitionToState(player.RunningState);
+        }
+    }
+}
